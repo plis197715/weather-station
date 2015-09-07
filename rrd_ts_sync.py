946e880eefb92b4047fe69c2115cab3938a99a16
ninjawil <ninjawil@users.noreply.github.com>
2015-09-07 13:35:12 +0100

    Add logging

--- rrd_ts_sync.py
+++ rrd_ts_sync.py
@@ -33,6 +33,7 @@
 #===============================================================================
 import rrdtool
 import thingspeak
+import settings as s
 
 
 
@@ -41,43 +42,82 @@ import thingspeak
 #===============================================================================
 def main():
 
+    #---------------------------------------------------------------------------
+    # System set up
+    #---------------------------------------------------------------------------
     rrdtool_file = s.RRDTOOL_RRD_FILE
     thingspeak_write_api_key = ''
     rrd_data = {}
     
+    logging.basicConfig(filename=s.LOG_FILENAME, level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+    logger = logging.getLogger(__name__)
+    logger.info('rrd_ts_sync Started')
+    
 
-    # --- Set up thingspeak account ---
-    thingspeak_ch = thingspeak.ThingspeakChannel(s.THINGSPEAK_HOST_ADDR,
-                                                    file=s.THINGSPEAK_API_KEY_FILENAME,
-                                                    ch_id=s.THINGSPEAK_CHANNEL_ID)
+    #---------------------------------------------------------------------------
+    # Set up thingspeak account
+    #---------------------------------------------------------------------------
+    ts_ch = thingspeak.ThingspeakChannel(s.THINGSPEAK_HOST_ADDR,
+                                            file=s.THINGSPEAK_API_KEY_FILENAME,
+                                            ch_id=s.THINGSPEAK_CHANNEL_ID)
 
-    # --- Interogate thingspeak for set up ---
+
+    #---------------------------------------------------------------------------
+    # Interogate thingspeak for set up
+    #---------------------------------------------------------------------------
     parameters = {'results':0}
-    r = thingspeak_ch.get_channel_feed(parameters)
+    r = ts_ch.get_channel_feed(parameters)
     last_entry_time = r['channels']['updated_at']
     field_names = r['channels']['field1']
 
-    # --- Check if RRD file exists ---
+
+    #---------------------------------------------------------------------------
+    # Check if RRD file exists
+    #---------------------------------------------------------------------------
     if not os.path.exists(rrdtool_file):
-        print('No RRA file found! Exiting...')
+        logger.error('No RRA file found! Exiting...')
         return
 
-    # --- Fetch values from rrd ---
+
+    #---------------------------------------------------------------------------
+    # Fetch values from rrd
+    #---------------------------------------------------------------------------
     data_values = rrdtool.fetch(rrdtool_file, 'LAST', 
                                 '-s', str(update_rate * -2))
+    logger.info(data_values)
 
-    # --- Check if RRD file variables match TS variables ---
+   
+    #---------------------------------------------------------------------------
+    # Check if RRD file variables match TS variables
+    #---------------------------------------------------------------------------
 
   
-    # --- Create a list with new thingspeak updates ---
+    #---------------------------------------------------------------------------
+    # Create a list with new thingspeak updates
+    #---------------------------------------------------------------------------
     
   
-    # --- Send data to thingspeak ---
-    response = thingspeak_ch.update_channel(sensor_data)
+    #---------------------------------------------------------------------------
+    # Send data to thingspeak
+    #---------------------------------------------------------------------------
+    response = ts_ch.update_channel(sensor_data)
+    logger.info('Thingspeak response %d', r.status_code)
 
 
-    # --- Check response from update attempt ---
-            
+    #---------------------------------------------------------------------------
+    # Check response from update attempt
+    #---------------------------------------------------------------------------
+    if response.status_code not requests.codes.ok:
+        sleep(5)
+        response = thingspeak_ch.update_channel(sensor_data)
+        logger.info('Thingspeak response %d', r.status_code)
+
+
+    #---------------------------------------------------------------------------
+    # Check response from update attempt
+    #---------------------------------------------------------------------------
+    logger.info('rrd_ts_sync finished')
 
 
 #===============================================================================
